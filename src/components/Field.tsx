eab019204d43b69b72988486445503db81e7cf83
David Riches <david.riches@appnovation.com>
2021-02-19 15:43:18 +0000

    fix: some refactoring

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,42 +1,57 @@
 // @ts-nocheck
 import { React, useState, useEffect } from "react";
-import { TextField } from "@contentful/forma-36-react-components";
+import {
+  Textarea,
+  TextField,
+  Form,
+} from "@contentful/forma-36-react-components";
 import { FieldExtensionSDK } from "@contentful/app-sdk";
 interface FieldProps {
   sdk: FieldExtensionSDK;
 }
 const Field = (props: FieldProps) => {
-  const [field, setField] = useState({ title: "", description: "" });
+  const [field, setField] = useState(
+    props.sdk.field.getValue() ?? { title: "", description: "" }
+  );
 
   useEffect(() => {
     props.sdk.window.startAutoResizer();
-    props.sdk.field.onValueChanged((value) => setField(value));
-  }, []);
+  });
+
+  const handleDescriptionChange = (e) => {
+    const value = {
+      ...field,
+      description: e.target.value,
+    };
+    props.sdk.field.setValue(value);
+    setField(value);
+  };
+
+  const handleTitleChange = (e) => {
+    const value = {
+      ...field,
+      title: e.target.value,
+    };
+    props.sdk.field.setValue(value);
+    setField(value);
+  };
 
   return (
     <section>
-      <TextField
-        labelText={"Title"}
-        onChange={(e) =>
-          props.sdk.field.setValue({
-            ...props.sdk.field.getValue(),
-            title: e.target.value,
-          })
-        }
-        value={field.title}
-        id="title"
-      />
-      <TextField
-        labelText={"Description"}
-        onChange={(e) =>
-          props.sdk.field.setValue({
-            ...props.sdk.field.getValue(),
-            description: e.target.value,
-          })
-        }
-        value={field.description}
-        id="description"
-      />
+      <Form>
+        <TextField
+          onChange={handleTitleChange}
+          textInputProps={{ placeholder: "Title" }}
+          value={field.title}
+          id="title"
+        />
+        <Textarea
+          placeholder="Description"
+          onChange={handleDescriptionChange}
+          value={field.description}
+          id="description"
+        />
+      </Form>
     </section>
   );
 };
